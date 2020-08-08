81666259aaa1eb8ecf73d1a1f4c3017377c8a33e
Owais javed <owaisjaved954@gmail.com>
2020-08-09 01:31:13 +0500

    Add files via upload

---
+++ Pharako_Challenge/build/precache-manifest.60e36a5f5dfdbf36b2f7d710e38cdf5f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23dff89558fdf9a6f7110852475944d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8526c442e4d3d1ad5a6",
+    "url": "/static/css/main.55f38019.chunk.css"
+  },
+  {
+    "revision": "f0f2d3b59b3424f0de1f",
+    "url": "/static/js/2.69f24deb.chunk.js"
+  },
+  {
+    "revision": "f3df11ca4cc8eaf0f270098bfa135497",
+    "url": "/static/js/2.69f24deb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8526c442e4d3d1ad5a6",
+    "url": "/static/js/main.5fdc1c1c.chunk.js"
+  },
+  {
+    "revision": "08f73d5cd9e484c26cef",
+    "url": "/static/js/runtime-main.7bb332b4.js"
+  }
+]);
